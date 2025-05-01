3a78f13acfd4a88c35415088adbb80198cc6256f
loganriggs <logan.smith.5@gmail.com>
2025-05-01 16:46:31 +0000

    Fixed embedding state dict  loading

--- utils.py
+++ utils.py
@@ -5,6 +5,7 @@ import torch.nn.functional as F
 from baukit import Trace
 from tqdm import tqdm
 import einops
+from einops import rearrange
 from huggingface_hub import HfApi, create_repo, login
 
 def push_sae_to_huggingface(
@@ -152,12 +153,13 @@ class AutoEncoderTopK(nn.Module):
         if 'per_token_bias.bias' in state_dict:
             num_tokens, d_model = state_dict["per_token_bias.bias"].shape
             embedding = EmbeddingBias(nn.Embedding(num_tokens, d_model))
-            del state_dict["per_token_bias.bias"]
+            # del state_dict["per_token_bias.bias"]
         else: 
             embedding = None
         autoencoder = AutoEncoderTopK(activation_dim, dict_size, k)
-        autoencoder.load_state_dict(state_dict)
         autoencoder.per_token_bias = embedding
+        autoencoder.load_state_dict(state_dict)
+        # autoencoder.per_token_bias = embedding
         if device is not None:
             autoencoder.to(device)
         return autoencoder
@@ -273,3 +275,182 @@ def calculate_fvu(x_orig, x_pred):
     numerator = torch.mean(torch.sum((x_orig - x_pred)**2, dim=-1))
     denominator = torch.mean(torch.sum((x_orig - mean)**2, dim=-1))
     return numerator / (denominator + 1e-6)
+
+
+'''Visualization Utils'''
+
+
+def make_colorbar(min_value, max_value, white = 255, red_blue_ness = 250, positive_threshold = 0.01, negative_threshold = 0.01):
+    # Add color bar
+    colorbar = ""
+    num_colors = 4
+    if(min_value < -negative_threshold):
+        for i in range(num_colors, 0, -1):
+            ratio = i / (num_colors)
+            value = round((min_value*ratio),1)
+            text_color = "255,255,255" if ratio > 0.5 else "0,0,0"
+            colorbar += f'<span style="background-color:rgba(255, {int(red_blue_ness-(red_blue_ness*ratio))},{int(red_blue_ness-(red_blue_ness*ratio))},1); color:rgb({text_color})">&nbsp{value}&nbsp</span>'
+    # Do zero
+    colorbar += f'<span style="background-color:rgba({white},{white},{white},1);color:rgb(0,0,0)">&nbsp0.0&nbsp</span>'
+    # Do positive
+    if(max_value > positive_threshold):
+        for i in range(1, num_colors+1):
+            ratio = i / (num_colors)
+            value = round((max_value*ratio),1)
+            text_color = "255,255,255" if ratio > 0.5 else "0,0,0"
+            colorbar += f'<span style="background-color:rgba({int(red_blue_ness-(red_blue_ness*ratio))},{int(red_blue_ness-(red_blue_ness*ratio))},255,1);color:rgb({text_color})">&nbsp{value}&nbsp</span>'
+    return colorbar
+
+def value_to_color(activation, max_value, min_value, white = 255, red_blue_ness = 250, positive_threshold = 0.01, negative_threshold = 0.01):
+    if activation > positive_threshold:
+        ratio = activation/max_value
+        text_color = "0,0,0" if ratio <= 0.5 else "255,255,255"  
+        background_color = f'rgba({int(red_blue_ness-(red_blue_ness*ratio))},{int(red_blue_ness-(red_blue_ness*ratio))},255,1)'
+    elif activation < -negative_threshold:
+        ratio = activation/min_value
+        text_color = "0,0,0" if ratio <= 0.5 else "255,255,255"  
+        background_color = f'rgba(255, {int(red_blue_ness-(red_blue_ness*ratio))},{int(red_blue_ness-(red_blue_ness*ratio))},1)'
+    else:
+        text_color = "0,0,0"
+        background_color = f'rgba({white},{white},{white},1)'
+    return text_color, background_color
+
+def convert_token_array_to_list(array):
+    if isinstance(array, torch.Tensor):
+        if array.dim() == 1:
+            array = [array.tolist()]
+        elif array.dim()==2:
+            array = array.tolist()
+        else: 
+            raise NotImplementedError("tokens must be 1 or 2 dimensional")
+    elif isinstance(array, list):
+        # ensure it's a list of lists
+        if isinstance(array[0], int):
+            array = [array]
+    return array
+
+def tokens_and_activations_to_html(toks, activations, tokenizer, logit_diffs=None, model_type="causal", text_above_each_act=None):
+    # text_spacing = "0.07em"
+    text_spacing = "0.00em"
+    toks = convert_token_array_to_list(toks)
+    activations = convert_token_array_to_list(activations)
+    # toks = [[tokenizer.decode(t).replace('Ġ', '&nbsp').replace('\n', '↵') for t in tok] for tok in toks]
+    toks = [[tokenizer.decode(t).replace('Ġ', '&nbsp').replace('\n', '\\n') for t in tok] for tok in toks]
+    highlighted_text = []
+    # Make background black
+    # highlighted_text.append('<body style="background-color:black; color: white;">')
+    highlighted_text.append("""
+<body style="background-color: black; color: white;">
+""")
+    max_value = max([max(activ) for activ in activations])
+    min_value = min([min(activ) for activ in activations])
+    if(logit_diffs is not None and model_type != "reward_model"):
+        logit_max_value = max([max(activ) for activ in logit_diffs])
+        logit_min_value = min([min(activ) for activ in logit_diffs])
+
+    # Add color bar
+    highlighted_text.append("Token Activations: " + make_colorbar(min_value, max_value))
+    if(logit_diffs is not None and model_type != "reward_model"):
+        highlighted_text.append('<div style="margin-top: 0.1em;"></div>')
+        highlighted_text.append("Logit Diff: " + make_colorbar(logit_min_value, logit_max_value))
+    
+    highlighted_text.append('<div style="margin-top: 0.5em;"></div>')
+    for seq_ind, (act, tok) in enumerate(zip(activations, toks)):
+        if(text_above_each_act is not None):
+            highlighted_text.append(f'<span>{text_above_each_act[seq_ind]}</span>')
+        for act_ind, (a, t) in enumerate(zip(act, tok)):
+            if(logit_diffs is not None and model_type != "reward_model"):
+                highlighted_text.append('<div style="display: inline-block;">')
+            text_color, background_color = value_to_color(a, max_value, min_value)
+            highlighted_text.append(f'<span style="background-color:{background_color};margin-right: {text_spacing}; color:rgb({text_color})">{t.replace(" ", "&nbsp")}</span>')
+            if(logit_diffs is not None and model_type != "reward_model"):
+                logit_diffs_act = logit_diffs[seq_ind][act_ind]
+                _, logit_background_color = value_to_color(logit_diffs_act, logit_max_value, logit_min_value)
+                highlighted_text.append(f'<div style="display: block; margin-right: {text_spacing}; height: 10px; background-color:{logit_background_color}; text-align: center;"></div></div>')
+        if(logit_diffs is not None and model_type=="reward_model"):
+            reward_change = logit_diffs[seq_ind].item()
+            text_color, background_color = value_to_color(reward_change, 10, -10)
+            highlighted_text.append(f'<br><span>Reward: </span><span style="background-color:{background_color};margin-right: {text_spacing}; color:rgb({text_color})">{reward_change:.2f}</span>')
+        highlighted_text.append('<div style="margin-top: 0.2em;"></div>')
+        # highlighted_text.append('<br><br>')
+    # highlighted_text.append('</body>')
+    highlighted_text = ''.join(highlighted_text)
+    return highlighted_text
+def save_token_display(tokens, activations, tokenizer, path, save=True, logit_diffs=None, show=False, model_type="causal"):
+    html = tokens_and_activations_to_html(tokens, activations, tokenizer, logit_diffs, model_type=model_type)
+    # if(save):
+    #     imgkit.from_string(html, path)
+    # if(show):
+    return display(HTML(html))
+
+def get_feature_indices(feature_activations, k=10, setting="max"):
+    # Sort the features by activation, get the indices
+    batch_size, seq_len = feature_activations.shape
+    feature_activations = rearrange(feature_activations, 'b s -> (b s)')
+    if setting=="max":
+        found_indices = torch.argsort(feature_activations, descending=True)[:k]
+    elif setting=="uniform":
+        # min_value = torch.min(feature_activations)
+        min_value = torch.min(feature_activations)
+        max_value = torch.max(feature_activations)
+
+        # Define the number of bins
+        num_bins = k
+
+        # Calculate the bin boundaries as linear interpolation between min and max
+        bin_boundaries = torch.linspace(min_value, max_value, num_bins + 1)
+
+        # Assign each activation to its respective bin
+        bins = torch.bucketize(feature_activations, bin_boundaries)
+
+        # Initialize a list to store the sampled indices
+        sampled_indices = []
+
+        # Sample from each bin
+        for bin_idx in torch.unique(bins):
+            if(bin_idx==0): # Skip the first one. This is below the median
+                continue
+            # Get the indices corresponding to the current bin
+            bin_indices = torch.nonzero(bins == bin_idx, as_tuple=False).squeeze(dim=1)
+            
+            # Randomly sample from the current bin
+            sampled_indices.extend(np.random.choice(bin_indices, size=1, replace=False))
+
+        # Convert the sampled indices to a PyTorch tensor & reverse order
+        found_indices = torch.tensor(sampled_indices).long().flip(dims=[0])
+    else: # random
+        # get nonzero indices
+        nonzero_indices = torch.nonzero(feature_activations)[:, 0]
+        # shuffle
+        shuffled_indices = nonzero_indices[torch.randperm(nonzero_indices.shape[0])]
+        found_indices = shuffled_indices[:k]
+    d_indices = found_indices // seq_len
+    s_indices = found_indices % seq_len
+    return d_indices, s_indices
+
+def get_feature_datapoints(d_idx, seq_pos_idx, all_activations, all_tokens, tokenizer, append=0):
+    full_activations = []
+    partial_activations = []
+    text_list = []
+    full_text = []
+    token_list = []
+    full_token_list = []
+    for md, s_ind in zip(d_idx, seq_pos_idx):
+        md = int(md)
+        s_ind = int(s_ind)
+        # full_tok = torch.tensor(dataset[md]["input_ids"])
+        
+        full_tok = all_tokens[md]
+        # [tokenizer.decode(t) for t in tokens[0]]
+
+        full_text.append(tokenizer.decode(full_tok))
+        # we want to add append more tokens, but only 
+        tok = full_tok[:s_ind+1+append]
+        # tok = dataset[md]["input_ids"][:s_ind+1]
+        full_activations.append(all_activations[md].tolist())
+        partial_activations.append(all_activations[md][:s_ind+1+append].tolist())
+        text = tokenizer.decode(tok)
+        text_list.append(text)
+        token_list.append(tok)
+        full_token_list.append(full_tok)
+    return text_list, full_text, token_list, full_token_list, partial_activations, full_activations
